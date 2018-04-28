b8a5cca514e0d98b14b8a7ea0fd61fde51679dfe
shivashankar2020 <shivashankar2020@gmail.com>
2018-04-28 20:22:59 +0100

    changes to file

--- HOffice/src/test/java/com/homeoffice/assessment/stepdefinitions/TBase.java
+++ HOffice/src/test/java/com/homeoffice/assessment/stepdefinitions/TBase.java
@@ -1,8 +1,5 @@
 package com.homeoffice.assessment.stepdefinitions;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.concurrent.TimeUnit;
 
@@ -10,66 +7,45 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import com.google.gson.Gson;
 import com.homeoffice.assessment.data.VehicleInformation;
-import com.homeoffice.assessment.files.AllFilesnotnull;
 
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 
 public class TBase {
 
-    protected WebDriver driver;
-    private boolean isInitialized;
-    private FileInputStream fileInputStream;
-    private Reader reader;
-    public VehicleInformation[] allVehiclesInformation;
-   
+	protected WebDriver driver;
+	private boolean isInitialized;
+	
+	private Reader reader;
+	public VehicleInformation[] allVehiclesInformation;
 
+	@Before
+	public void initializeEnvironment() {
+		if (!isInitialized) {
 
-    @Before
-    public void  initializeEnvironment(){
-        if(!isInitialized) {
-            File file = new File(System.getProperty("user.dir")+"//report.html");
-            file.delete();
-            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//chromedriver.exe");
-            ChromeOptions chromeOptions = new ChromeOptions();
-            chromeOptions.addArguments("--start-maximized");
-            driver = new ChromeDriver(chromeOptions);
-            driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-            isInitialized = true;
-            AllFilesnotnull excelToJSON = new AllFilesnotnull();
-            excelToJSON.convertExcelToJsonTestData("Files", "sheet");
-            try {
-                fileInputStream = new FileInputStream(new File(System.getProperty("user.dir") + "\\src\\test\\testdata\\Vehicle Information.json"));
-                reader = new InputStreamReader(fileInputStream, "UTF8");
-                allVehiclesInformation = new Gson().fromJson(reader, VehicleInformation[].class);
-                fileInputStream.close();
-                reader.close();
-            } catch (Exception e){
-                e.printStackTrace();
-            }
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//chromedriver.exe");
+			ChromeOptions chromeOptions = new ChromeOptions();
+			chromeOptions.addArguments("--start-maximized");
+			driver = new ChromeDriver(chromeOptions);
+			driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			
 
+		}
+	}
 
-        }
-    }
+	public WebDriver getDriver() {
+		return driver;
+	}
 
-    public WebDriver getDriver() {
-        return driver;
-    }
+	public VehicleInformation[] getAllVehiclesInformation() {
+		return allVehiclesInformation;
+	}
 
-    public VehicleInformation[] getAllVehiclesInformation() {
-        return allVehiclesInformation;
-    }
+	@After
+	public void tearDownEnvironment() {
+		driver.quit();
 
-    
-
-   
-
-    @After
-    public void tearDownEnvironment(){
-        driver.quit();
-        
-    }
+	}
 }
